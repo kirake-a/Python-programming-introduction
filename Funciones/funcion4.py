999a494621eafff74748e87518fe28b1f476c412
Rubén Alvarado <92123464+kirake-a@users.noreply.github.com>
2023-03-01 13:20:41 -0600

    Calculando los pares dado un tope

--- Funciones/funcion4.py
+++ Funciones/funcion4.py
@@ -1,2 +1,11 @@
 # Calculando a los primeros numeros pares
+print("Los numeros pares en los primeros 100 numeros")
 
+def primero_pares(tope):
+    # for en i, desde 0 hasta tope-1
+    for i in range(0, tope+1):
+
+        if (i % 2 == 0):
+            print(i, end = " ")
+
+primero_pares(100)
